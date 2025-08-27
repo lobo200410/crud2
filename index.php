d104375c0ee53584ccdf12db7d3a80fa914fdc8c
René Cruz <renelobos12@gmail.com>
2025-08-27 14:34:05 -0600

    dandole forma al index.php raiz

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<h1>login</h1>
+<h1>Login</h1>
+<h2>Please enter your credentials</h2>
+<form method="post" action="login.php">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <input type="submit" value="Login">
+</form>
